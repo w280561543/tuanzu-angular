8656c3b1d658cead097a5b0e76fa1060207d0366
Maing <280561543@qq.com>
2016-09-15 16:40:08 +0800

    Update: 修改Angular2 架构。

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,7 @@
 import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-
-import { DashboardComponent } from './dashboard.component';
-import { LoginComponent } from './auth/login.component';
-
-import { Suite.IndexComponent } from './suite/index.component';
 
 @Component({
     selector: 'app',
-    template: '<router-outlet></router-outlet>',
-	directives: [ROUTER_DIRECTIVES],
-	providers: [
-		ROUTER_PROVIDERS
-	]
+    template: '<router-outlet></router-outlet>'
 })
-
-@RouteConfig([
-	{ path: '/dashboard', name: 'Dashboard', component: DashboardComponent,	useAsDefault: true },
-	{ path: '/login', name: 'Login', component: LoginComponent },
-	
-	{ path: '/suite', name: 'SuiteIndex', component: Suite.IndexComponent }
-])
-
 export class AppComponent { }
