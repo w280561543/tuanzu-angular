cea436d579799718bcb56a3bbde6dc883b3a1dc8
Maing <280561543@qq.com>
2016-10-07 23:05:17 +0800

    Create: 创建login.service，从login.component中分离。

--- app/auth/login.component.ts
+++ app/auth/login.component.ts
@@ -1,9 +1,7 @@
-import { Component }        from '@angular/core';
-import { Router,
-		 NavigationExtras,
-}							from '@angular/router';
+import { Component } from '@angular/core';
 
-import { AuthService } from './auth.service';
+import { LoginService } from './login.service';
+import { Login }        from './login.model';
 
 @Component({
 	moduleId: module.id,
@@ -12,42 +10,12 @@ import { AuthService } from './auth.service';
 })
 
 export class LoginComponent {
-	public message: string;
+	public model: Login = new Login('', '');
 	
 	public constructor(
-		public authService: AuthService,
-		public router: Router
-	) {
-		this.setMessage();
-	}
-	
-	public setMessage() {
-		this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
-	}
-	
-	public login() {
-		this.message = 'Trying to log in ...';
-		
-		this.authService.login().subscribe(() => {
-			this.setMessage();
-			if (this.authService.isLoggedIn) {
-				let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/dashboard';
-				
-				// Set our navigation extras object
-				// that passes on our global query params and fragment
-				let navigationExtras: NavigationExtras = {
-					preserveQueryParams: true,
-					preserveFragment: true
-				};
-				
-				// Redirect the user
-				this.router.navigate([redirect], navigationExtras);
-			}
-		});
-	}
-	
-	public logout() {
-		this.authService.logout();
-		this.setMessage();
-	}
+		public loginService: LoginService
+	) {}
+
+	public onSubmit() {
+	};
 }
---
+++ app/auth/login.service.ts
@@ -0,0 +1,47 @@
+import { Injectable }       from '@angular/core';
+import { Router,
+		 NavigationExtras
+}							from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class LoginService {
+	public message: string;
+	
+	public constructor(
+		public authService: AuthService,
+		public router: Router
+	) {
+		this.setMessage();
+	}
+	
+	public setMessage() {
+		this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+	}
+	
+	public login() {
+		this.message = 'Trying to log in ...';
+
+		this.authService.login().subscribe(() => {
+			this.setMessage();
+			if(this.authService.isLoggedIn) {
+				let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/dashboard';
+
+				// Set our navigation extras object
+				// that passes on our global query params and fragment
+				let navigationExtras: NavigationExtras = {
+					preserveQueryParams: true,
+					preserveFragment: true
+				};
+
+				// Redirect the user
+				this.router.navigate([redirect], navigationExtras);
+			}
+		});
+	}
+
+	public logout() {
+		this.authService.logout();
+		this.setMessage();
+	}
+}
