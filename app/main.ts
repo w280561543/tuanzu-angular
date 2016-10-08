0058d38b86eb7d8a7e7222b73970b4603da62ffe
Maing <280561543@qq.com>
2016-10-08 15:21:16 +0800

    Add: 添加备注。

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,8 @@
+// The browser platform with a compiler
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from 'app/app.module';
+// The app module
+import { AppModule } from './app.module';
 
+// Compile and launch the module
 platformBrowserDynamic().bootstrapModule(AppModule);
