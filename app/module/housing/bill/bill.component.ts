b3a959af86199daf62ccd79426c1128c766b6548
Maing <280561543@qq.com>
2016-10-14 18:14:10 +0800

    Update: 修改bill名为housing-bill

--- app/module/housing/bill/bill.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-	selector: 'housing-bill',
-	templateUrl: 'app/housing/bill/bill.component.html'
-})
-
-export class BillComponent { }
---
+++ app/module/housing/bill/housing-bill.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'housing-bill',
+	templateUrl: 'housing-bill.component.html'
+})
+
+export class HousingBillComponent { }
