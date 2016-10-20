38c89e5c3a84353e74642d90fddb89c43e33eb9b
Maing <280561543@qq.com>
2016-10-20 17:55:02 +0800

    Update: 更新布局。

--- app/module/housing/housing.component.ts
+++ app/module/housing/housing.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-	template: `<router-outlet></router-outlet>`
+	template: `
+	<div class="container-fluid">
+		<div class="row">
+			<div class="hidden-sm-down col-md-2">
+				Sidebarsss
+			</div>
+			<div class="col-sm-12 col-md-10 col-md-offset-2">
+				<router-outlet></router-outlet>
+			</div>
+		</div>
+	</div>`
 })
 
 export class HousingComponent{
