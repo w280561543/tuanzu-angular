f1d14e7532a96f61a2ad5c868be4356f6dfdf6e0
Maing <280561543@qq.com>
2016-10-22 16:35:27 +0800

    Chore: 修复

--- app/module/housing/housing.module.ts
+++ app/module/housing/housing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }   from '@angular/core';
 import { HttpModule } from '@angular/http';
 
-import { SharedModule } from 'app/shared/shared.module';
+import { SharedModule } from '../../shared/shared.module';
 
 import { housingRouting } from './housing.routing';
 
