6a49b470f620e1b8275e7ae644e2bd3164a103e7
Maing <280561543@qq.com>
2016-10-22 10:55:59 +0800

    Chore: 修改代码位置。

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpModule }    from '@angular/http';
 
 import { SharedModule }  from './shared/shared.module';
 
-import { AppComponent }        from './app.component';
 import { routing, 
 		 appRoutingProviders } from './app.routing';
+
+import { AppComponent }        from './app.component';
 
 import { LoginComponent }     from './auth/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
