cb76b1a3ca34df8f47937774b8acfd5756033129
Maing <280561543@qq.com>
2016-10-23 18:08:30 +0800

    Update: sidebar功能开发中，未完成。

--- app/shared/sidebar/sidebar.component.ts
+++ app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	moduleId: module.id,
@@ -8,8 +8,10 @@ import { Component, HostListener } from '@angular/core';
 			<li class="nav-item">
 				<a class="nav-link" routerLink="/dashboard" routerLinkActive="active">首页</a>
 			</li>
+
 			<li class="small text-muted">管理</li>
-			<li class="nav-item dropdown" (click)="toggle($event)" [class.open]="isOpen">
+
+			<li class="nav-item dropdown" (click)="toggle($event, 0)" [class.open]="isOpen[0]">
 				<a class="nav-link dropdown-toggle" href="javascript:void(0);">Housing</a>
 				<div class="dropdown-menu">
 					<a class="dropdown-item" routerLink="/housing" routerLinkActive="active" [routerLinkActiveOptions]="{exact:
@@ -17,18 +19,25 @@ true}">Housing</a>
 					<a class="dropdown-item" routerLink="/housing/bill" routerLinkActive="active">Housing-bill</a>
 				</div>
 			</li>
+
+			<li class="nav-item dropdown" (click)="toggle($event, 1)" [class.open]="isOpen[1]">
+				<a class="nav-link dropdown-toggle" href="javascript:void(0);">Room</a>
+				<div class="dropdown-menu">
+					<a class="dropdown-item" href="javascript:void(0);">Room</a>
+					<a class="dropdown-item" href="javascript:void(0);">Room-bill</a>
+				</div>
+			</li>
+
 		</ul>
     `,
     styleUrls: ['sidebar.component.css']
 })
 
 export class SidebarComponent {
-	public isOpen: boolean = false;
-	
-	@HostListener('click', ['$event.target.offsetParent'])
-	public toggle(ev: Any): void {
-		//ev.stopPropagation();
-		console.log(ev);
-		this.isOpen = !this.isOpen;
+	public isOpen: any[] = [false, false];
+
+	public toggle(e: Event, n: number): void {
+		e.stopPropagation();
+		this.isOpen[n] = !this.isOpen[n];
 	}
 }
