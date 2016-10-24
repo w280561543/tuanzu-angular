72d1a8aab85670c33ba8426ca441bd2b73ce5dc9
Maing <280561543@qq.com>
2016-10-24 11:13:53 +0800

    Chore: 常规修复.

--- app/module/housing/housing.service.ts
+++ app/module/housing/housing.service.ts
@@ -1,28 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http }       from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
-export class HousingService {
-
-	public constructor(private http: Http) {}
-
-	public getAll() {
-		/*
-		return this.http.get('http://127.0.0.1:8080/suite?page=0&size=20')
-			.toPromise()
-			.then(response => response.json())
-			.catch(this.handleError);
-		*/
-	}
-
-	public getId(id: number) {
-		console.log(id);
-	}
-	
-	private handleError(error: any) {
-		console.error('An error occurred', error);
-		return Promise.reject(error.message || error);
-	}
-}
+export class HousingService {}
