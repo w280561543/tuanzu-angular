269fef6d6f9362127abb3e68eb56c294b2af6745
Maing <280561543@qq.com>
2016-11-05 18:03:50 +0800

    Update: 编写housing-room-contract模板.

--- app/modules/housing-room/contract/housing-room-contract.component.ts
+++ app/modules/housing-room/contract/housing-room-contract.component.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	moduleId: module.id,
 	templateUrl: 'housing-room-contract.component.html'
 })
-export class HousingRoomContractComponent {}
+export class HousingRoomContractComponent implements OnInit {
+	public step: Array < any > ;
+
+	public ngOnInit(): void {
+		this.step = [{
+			active: true,
+		}, {
+			active: false,
+		}, {
+			active: false,
+		}];
+	}
+
+	public active(n: number): boolean {
+		if(!this.step[n].active) {
+			for(let i in this.step) {
+				this.step[i].active = false;
+			}
+			this.step[n].active = true;
+		}
+	}
+
+}
