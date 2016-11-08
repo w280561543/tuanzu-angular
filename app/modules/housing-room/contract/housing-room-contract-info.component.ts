9a47c329918cfd20beb1697dc6be2eeb793cf25f
Maing <280561543@qq.com>
2016-11-08 18:02:46 +0800

    Chore: 编写数据展示.

--- app/modules/housing-room/contract/housing-room-contract-info.component.ts
+++ app/modules/housing-room/contract/housing-room-contract-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { HousingRoomContractService } from './housing-room-contract.service';
 
 @Component({
 	moduleId: module.id,
@@ -7,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HousingRoomContractInfoComponent implements OnInit {
+	public constructor(
+		private _route: ActivatedRoute
+		private _housingRoomContractService: HousingRoomContractService
+	) {}
+
 	public ngOnInit(): void {
-		console.log('HousingRoomContractInfoComponent');
+		this._housingRoomContractService.getOne(this._route.params.value.housing_room_id)
+			.subscribe(r => {
+				console.log(r);
+			});
 	}
 }
