5210f882964a81e954034d84366f0ed63d6a06f5
Maing <280561543@qq.com>
2016-12-28 18:00:25 +0800

    Update: 删除搜索,只保留获取数据,暂不作其他操作.

--- app/modules/housing/housing.service.ts
+++ app/modules/housing/housing.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, URLSearchParams } from '@angular/http';
+import { Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
@@ -8,31 +8,13 @@ import 'rxjs/add/operator/catch';
 export class HousingService {
 	private _url: string = 'http://api.52tuanzu.com/v1';
 
-	private urlSearchParams: URLSearchParams;
-
 	public constructor(
 		private _http: Http
-	) {
-		this.urlSearchParams = new URLSearchParams();
-	}
-
-	public setPage(v: any): void {
-		this.urlSearchParams.set('page', v);
-	}
-
-	public setFilters(v: string): void {
-		this.urlSearchParams.set('filters', v);
-	}
-	
-	public deleteFilters(): void {
-		this.urlSearchParams.delete('filters');
-	}
+	) {}
 
 	public getAll(): Observable < any > {
 		return this._http
-			.get(this._url + '/housing', {
-				search: this.urlSearchParams
-			})
+			.get(this._url + '/housing')
 			.catch(this.handleError);
 	}
 
