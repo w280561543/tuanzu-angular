7ac21d0dbe3bbf860f41375748b91aa6a0052245
Maing <280561543@qq.com>
2017-03-31 21:19:09 +0800

    Update: 合同变更模板.

--- src/app/modules/housing-room/contract/housing-room-contract-update.component.ts
+++ src/app/modules/housing-room/contract/housing-room-contract-update.component.ts
@@ -1,7 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { HousingRoomContractService } from './housing-room-contract.service';
 
 @Component({
 	moduleId: module.id,
 	templateUrl: 'housing-room-contract-update.component.html'
 })
-export class HousingRoomContractUpdateComponent {}
+export class HousingRoomContractUpdateComponent implements OnInit {
+	private step: Array < any > = [{},{},{},{
+		active: true,
+	}, {
+		active: false,
+	}, {
+		active: false,
+	}];
+
+	private data: Object = {};
+
+	public constructor(
+		private _route: ActivatedRoute,
+		private _router :Router,
+		private _housingRoomContractService: HousingRoomContractService
+	) {}
+	
+	public ngOnInit(): void {
+		this._housingRoomContractService
+			.getOne(this._route.params['value'].housing_room_id)
+			.subscribe(r => {
+				this.data = r.json().data;
+				this._housingRoomContractService.setData(this.data);
+			});
+	}
+}
