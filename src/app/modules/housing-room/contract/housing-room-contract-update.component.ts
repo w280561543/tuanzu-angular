5639e6cc631e8beed73380abffec6c6b26379bbb
Maing <280561543@qq.com>
2017-03-31 23:45:18 +0800

    Update: 增加提交功能.

--- src/app/modules/housing-room/contract/housing-room-contract-update.component.ts
+++ src/app/modules/housing-room/contract/housing-room-contract-update.component.ts
@@ -17,13 +17,17 @@ export class HousingRoomContractUpdateComponent implements OnInit {
 	}];
 
 	private data: Object = {};
+	
+	// 水电燃气
+	private resource: Object = {};
+	private model: Object = {};
 
 	public constructor(
 		private _route: ActivatedRoute,
 		private _router :Router,
 		private _housingRoomContractService: HousingRoomContractService
 	) {}
-	
+
 	public ngOnInit(): void {
 		this._housingRoomContractService
 			.getOne(this._route.params['value'].housing_room_id)
@@ -31,5 +35,18 @@ export class HousingRoomContractUpdateComponent implements OnInit {
 				this.data = r.json().data;
 				this._housingRoomContractService.setData(this.data);
 			});
+	}
+
+	public onActive(n: number): void {
+		if(!this.step[n].active) {
+			for(let i in this.step) {
+				this.step[i].active = false;
+			}
+			this.step[n].active = true;
+		}
+	}
+	
+	public submitResource(): void {
+		console.log(this.resource);
 	}
 }
